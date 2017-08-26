bb0a449ecd4b900f68093c96593ac07dbb945754
Duong Tuan Luc <luc@ltv.vn>
2017-08-26 07:52:49 +0700

    Update HomeController.js

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -11,21 +11,6 @@ const getIndex = (req, res) => {
   res.render('home/index', model)
 }
 
-const getDownloadHistories = async (req, res) => {
-  try {
-    const histories = await service.fetchDownloadHistories()
-    // console.log(`Download histories: `, histories)
-    res.status(200).json({
-      today: moment().format('DD-MM-YYYY'),
-      histories,
-    })
-  } catch (err) {
-    res.status(500).json(err)
-    console.log(`getDownloadHistories: `, err)
-  }
-}
-
 module.exports = {
   getIndex,
-  getDownloadHistories,
 };
